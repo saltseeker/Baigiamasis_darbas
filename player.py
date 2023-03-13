80053ee46bf013f6e44de5ead423d66b6a570d71
saltseeker <soulseekas@gmail.com>
2023-03-13 10:44:20 +0200

    added - requirments, player - seperated player vertical horizontal movement

--- player.py
+++ player.py
@@ -33,8 +33,17 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 0
 
     def move(self,dt):
-        self.pos += self.direction * self.speed * dt
-        self.rect.center = self.pos
+
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+
+        # horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+    
+        # vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
     
     def update(self, dt):
         self.input()
