3e055a84261f5e88c5019e901ab0dbf85a28a43e
saltseeker <soulseekas@gmail.com>
2023-03-14 10:21:39 +0200

    Overlay - added more

--- overlay.py
+++ overlay.py
@@ -16,3 +16,7 @@ class Overlay:
         tool_surf = self.tools_surf[self.player.selected_tool]
         tool_rect = tool_surf.get_rect(midbottom = OVERLAY_POSITIONS['tool'])
         self.display_surface.blit(tool_surf,tool_rect)
+
+        seed_surf = self.seeds_surf[self.player.selected_seed]
+        seed_rect = seed_surf.get_rect(midbottom = OVERLAY_POSITIONS['seed'])
+        self.display_surface.blit(seed_surf, seed_rect)
