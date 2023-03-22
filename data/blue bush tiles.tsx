a7a4efba5f080470a03d0e9b1cba2de421dcf8ff
saltseeker <soulseekas@gmail.com>
2023-03-22 08:45:42 +0200

    fixed mistake

--- data/blue bush tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="blue bush tiles" tilewidth="128" tileheight="128" tilecount="1" columns="1">
- <image source="../../../Users/souls/Desktop/Sprout Lands - Sprites - premium pack/tilesets/Ground tiles/new tiles/blue bush tiles.png" width="176" height="192"/>
-</tileset>
