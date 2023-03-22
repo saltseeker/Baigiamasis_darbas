6f1f7696200ca1bb4d12e28231abab23a3696b24
saltseeker <soulseekas@gmail.com>
2023-03-22 08:41:47 +0200

    map, collision, trees, particles, inventory, cycle

--- settings.py
+++ settings.py
@@ -1,15 +1,13 @@
 from pygame.math import Vector2
-
 # screen
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TILE_SIZE = 64
 
-
-# overlay positions
+# overlay positions 
 OVERLAY_POSITIONS = {
-    'tool' : (40, SCREEN_HEIGHT - 15),
-    'seed' : (70, SCREEN_HEIGHT - 5)}
+	'tool' : (40, SCREEN_HEIGHT - 15), 
+	'seed': (70, SCREEN_HEIGHT - 5)}
 
 PLAYER_TOOL_OFFSET = {
 	'left': Vector2(-50,40),
@@ -32,4 +30,23 @@ LAYERS = {
 	'rain drops': 10
 }
 
+APPLE_POS = {
+	'Small': [(18,17), (30,37), (12,50), (30,45), (20,30), (30,10)],
+	'Large': [(30,24), (60,65), (50,50), (16,40),(45,50), (42,70)]
+}
 
+GROW_SPEED = {
+	'corn': 1,
+	'tomato': 0.7
+}
+
+SALE_PRICES = {
+	'wood': 4,
+	'apple': 2,
+	'corn': 10,
+	'tomato': 20
+}
+PURCHASE_PRICES = {
+	'corn': 4,
+	'tomato': 5
+}
