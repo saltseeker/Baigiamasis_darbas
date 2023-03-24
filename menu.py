26b521cb39f778af70ce31e639d7970d384dcf9d
saltseeker <soulseekas@gmail.com>
2023-03-24 16:12:00 +0200

    testing menu inteface

--- menu.py
+++ menu.py
@@ -21,9 +21,17 @@ class Menu:
     def setup(self):
         #text surface
         self.text_surfs = []
+        self.total_height = 0
+        
+        #menu brackets
         for item in self.options:
             text_surf = self.font.render(item, False, 'Black')
             self.text_surfs.append(text_surf)
+            self.total_height += text_surf.get_height() + (self.padding * 2)
+
+        self.total_height += (len(self.text_surfs) - 1) * self.space
+        self.menu_top = SCREEN_HEIGHT / 2 - self.total_height / 2
+        self.main_rect = pygame.Rect(SCREEN_WIDTH / 2 - self.width / 2,self.menu_top,self.width,self.total_height)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -34,6 +42,7 @@ class Menu:
 
     def update(self):
         self.input()
-        for text_index, text_surf in enumerate(self.text_surfs):
-            self.display_surface.blit(text_surf,(100,text_index * 50))
+        pygame.draw.rect(self.display_surface,'red', self.main_rect)
+        #for text_index, text_surf in enumerate(self.text_surfs):
+            #self.display_surface.blit(text_surf,(100,text_index * 50))
         
