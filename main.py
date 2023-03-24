5be7df0d07153ef4eab1c44474a99afa139dcf57
saltseeker <soulseekas@gmail.com>
2023-03-24 21:56:40 +0200

    Added start menu

--- main.py
+++ main.py
@@ -3,26 +3,53 @@ from settings import *
 from level import Level
 
 
+
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption('Farm land')
         self.clock = pygame.time.Clock()
         self.level = Level()
+        self.font = pygame.font.Font('font/LycheeSoda.ttf', 24)
+        self.start_image = pygame.image.load('graphics/world/start.gif')
 
     def instruction(self):
-        font = pygame.font.Font('font/LycheeSoda.ttf', 24)
         text = " Q = swap tool\n E = swap seed\n SPACE = action key\n X = plant"
         lines = text.splitlines()
         y = 20
         for line in lines:
-            text_surf = font.render(line, False, pygame.Color('black'))
+            text_surf = self.font.render(line, False, pygame.Color('black'))
             text_rect = text_surf.get_rect(topleft=(20, y))
             self.screen.blit(text_surf, text_rect)
             y += 30
         
+    def start_screen(self):
+        while True:
+            start_image_scaled = pygame.transform.scale(self.start_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+            self.screen.blit(start_image_scaled, (0, 0))
+
+            title_surf = self.font.render('Farm land', False, pygame.Color('black'))
+            title_rect = title_surf.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/3))
+            self.screen.blit(title_surf, title_rect)
+
+            start_surf = self.font.render('Press ENTER to start', False, pygame.Color('black'))
+            start_rect = start_surf.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+            self.screen.blit(start_surf, start_rect)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                    return
+
+            pygame.display.update()
+            self.clock.tick(60)
+
     def run(self):
+        self.start_screen()
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -33,7 +60,6 @@ class Game:
             self.level.run(dt)
             self.instruction()
             pygame.display.update()
-
 if __name__ == '__main__':
     game = Game()
     game.run()
