43793a827c8a2010beb61aa61a96125a13c19ecd
saltseeker <soulseekas@gmail.com>
2023-03-24 15:52:45 +0200

    testing menu options

--- menu.py
+++ menu.py
@@ -9,6 +9,14 @@ class Menu:
         self.display_surface = pygame.display.get_surface()
         self.font = pygame.font.Font(f'font/QuinqueFive.ttf', 30)
 
+        #menu size
+        self.width = 400
+        self.space = 10
+        self.padding = 8 
+
+        self.options = list(self.player.item_inventory.keys()) + list(self.player.seed_inventory.keys())
+        print(self.options)
+
     def input(self):
         keys = pygame.key.get_pressed()
 
